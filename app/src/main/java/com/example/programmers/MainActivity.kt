4255a2ab2818f61e606e2755f2e06c17b2b89335
park-hwaan <parkhwan1129@gmail.com>
2024-08-03 17:53:46 +0900

    대충만든자판

--- app/src/main/java/com/example/programmers/MainActivity.kt
+++ app/src/main/java/com/example/programmers/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.programmers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            delay(1000L)
+            println("World")
+        }
+        println("Hello")
+        runBlocking {
+            delay(2000L)
+        }
+
     }
 }
